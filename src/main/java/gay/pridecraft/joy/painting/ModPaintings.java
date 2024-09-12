02146b2d30f54ae05348ef35f7d87e03b05a0d99
blryface <124838505+blryface@users.noreply.github.com>
2024-09-12 20:42:22 -0300

    Update Pridepack assets, add new paintings

--- src/main/java/gay/pridecraft/joy/painting/ModPaintings.java
+++ src/main/java/gay/pridecraft/joy/painting/ModPaintings.java
@@ -8,22 +8,27 @@ import net.minecraft.util.Identifier;
 import static gay.pridecraft.joy.Joy.MOD_ID;
 
 public class ModPaintings {
-    public static final RegistryKey<PaintingVariant> ACE = of("ace");
-    public static final RegistryKey<PaintingVariant> AGENDER = of("agender");
-    public static final RegistryKey<PaintingVariant> ARO = of("aro");
-    public static final RegistryKey<PaintingVariant> AROACE = of("aroace");
-    public static final RegistryKey<PaintingVariant> BIGENDER = of("bigender");
-    public static final RegistryKey<PaintingVariant> BISEXUAL = of("bisexual");
-    public static final RegistryKey<PaintingVariant> ENBY = of("enby");
-    public static final RegistryKey<PaintingVariant> GAY = of("gay");
-    public static final RegistryKey<PaintingVariant> INTERSEX = of("intersex");
-    public static final RegistryKey<PaintingVariant> LESBIAN = of("lesbian");
-    public static final RegistryKey<PaintingVariant> MLM = of("mlm");
-    public static final RegistryKey<PaintingVariant> PAN = of("pan");
-    public static final RegistryKey<PaintingVariant> PROGRESS = of("progress");
-    public static final RegistryKey<PaintingVariant> QUEER = of("queer");
-    public static final RegistryKey<PaintingVariant> STRAIGHT_ALLY = of("straight_ally");
-    public static final RegistryKey<PaintingVariant> TRANS = of("trans");
+    public static final RegistryKey<PaintingVariant> ACE =              of("ace");
+    public static final RegistryKey<PaintingVariant> AGENDER =          of("agender");
+    public static final RegistryKey<PaintingVariant> ARO =              of("aro");
+    public static final RegistryKey<PaintingVariant> AROACE =           of("aroace");
+    public static final RegistryKey<PaintingVariant> BIGENDER =         of("bigender");
+    public static final RegistryKey<PaintingVariant> BISEXUAL =         of("bisexual");
+    public static final RegistryKey<PaintingVariant> ENBY =             of("enby");
+    public static final RegistryKey<PaintingVariant> GAY =              of("gay");
+    public static final RegistryKey<PaintingVariant> INTERSEX =         of("intersex");
+    public static final RegistryKey<PaintingVariant> LESBIAN =          of("lesbian");
+    public static final RegistryKey<PaintingVariant> MLM =              of("mlm");
+    public static final RegistryKey<PaintingVariant> PAN =              of("pan");
+    public static final RegistryKey<PaintingVariant> PROGRESS =         of("progress");
+    public static final RegistryKey<PaintingVariant> QUEER =            of("queer");
+    public static final RegistryKey<PaintingVariant> STRAIGHT_ALLY =    of("straight_ally");
+    public static final RegistryKey<PaintingVariant> TRANS =            of("trans");
+    public static final RegistryKey<PaintingVariant> BIRB =             of("birb");
+    public static final RegistryKey<PaintingVariant> DEMIROMANTIC =     of("demiromantic");
+    public static final RegistryKey<PaintingVariant> DEMISEXUAL =       of("demisexual");
+    public static final RegistryKey<PaintingVariant> FLOATING_TREES =   of("floating_trees");
+    public static final RegistryKey<PaintingVariant> FLOATING_BIIS =    of("floating_biis");
 
     private static RegistryKey<PaintingVariant> of(String id) {
         return RegistryKey.of(RegistryKeys.PAINTING_VARIANT, Identifier.of(MOD_ID, id));
