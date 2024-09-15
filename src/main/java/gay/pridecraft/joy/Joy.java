7eef422965babfe24d93cefd31c63e0f2ed11f01
blryface <124838505+blryface@users.noreply.github.com>
2024-09-15 19:04:56 -0300

    PrideSlime -> 🪦 2: Eletric Boogaloo

--- src/main/java/gay/pridecraft/joy/Joy.java
+++ src/main/java/gay/pridecraft/joy/Joy.java
@@ -97,10 +97,6 @@ public class Joy implements ModInitializer {
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 10.0)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.0));
 
-        FabricDefaultAttributeRegistry.register(ModEntities.PRIDE_SLIME, MobEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2.0)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3));
-
         FabricDefaultAttributeRegistry.register(ModEntities.PRIDE_SNIFFER, MobEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.1));
     }
