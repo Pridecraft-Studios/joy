99fe0c9d9f8e87c238dad3331308863c31774162
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2024-11-19 23:10:13 +1100

    fix spawn egg datagen

--- src/main/java/gay/pridecraft/joy/datagen/JoyModelProvider.java
+++ src/main/java/gay/pridecraft/joy/datagen/JoyModelProvider.java
@@ -4,9 +4,10 @@ import gay.pridecraft.joy.registry.JoyBlocks;
 import gay.pridecraft.joy.registry.JoyItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.Models;
+import net.minecraft.data.client.*;
+import net.minecraft.util.Identifier;
+
+import java.util.Optional;
 
 public class JoyModelProvider extends FabricModelProvider {
     public JoyModelProvider(FabricDataOutput output) {
@@ -39,13 +40,15 @@ public class JoyModelProvider extends FabricModelProvider {
         itemModelGenerator.register(JoyItems.TOTEM_OF_PRIDE, Models.GENERATED);
         itemModelGenerator.register(JoyItems.PRIDE_ELYTRA, Models.GENERATED);
         itemModelGenerator.register(JoyItems.PRIDE_BRUSH, Models.GENERATED);
-        itemModelGenerator.register(JoyItems.SOCK_FOX_SPAWN_EGG, Models.GENERATED);
-        itemModelGenerator.register(JoyItems.BII_SPAWN_EGG, Models.GENERATED);
-        itemModelGenerator.register(JoyItems.ENBEE_SPAWN_EGG, Models.GENERATED);
-        itemModelGenerator.register(JoyItems.TRANS_BEE_SPAWN_EGG, Models.GENERATED);
-        itemModelGenerator.register(JoyItems.FROG_SPAWN_EGG, Models.GENERATED);
-        itemModelGenerator.register(JoyItems.SNIFFER_SPAWN_EGG, Models.GENERATED);
+        itemModelGenerator.register(JoyItems.SOCK_FOX_SPAWN_EGG, TEMPLATE_SPAWN_EGG);
+        itemModelGenerator.register(JoyItems.BII_SPAWN_EGG, TEMPLATE_SPAWN_EGG);
+        itemModelGenerator.register(JoyItems.ENBEE_SPAWN_EGG, TEMPLATE_SPAWN_EGG);
+        itemModelGenerator.register(JoyItems.TRANS_BEE_SPAWN_EGG, TEMPLATE_SPAWN_EGG);
+        itemModelGenerator.register(JoyItems.FROG_SPAWN_EGG, TEMPLATE_SPAWN_EGG);
+        itemModelGenerator.register(JoyItems.SNIFFER_SPAWN_EGG, TEMPLATE_SPAWN_EGG);
         itemModelGenerator.register(JoyItems.TEST_DISC, Models.GENERATED);
         itemModelGenerator.register(JoyItems.TEST_DISK, Models.GENERATED);
     }
+
+    public static final Model TEMPLATE_SPAWN_EGG = new Model(Optional.of(Identifier.ofVanilla("item/template_spawn_egg")), Optional.empty());
 }
