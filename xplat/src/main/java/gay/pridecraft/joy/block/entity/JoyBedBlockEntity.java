890492ea63e679c4f701bc9217b93e6d609d6d9a
Ampflower 🌺 <git@ampflower.gay>
2025-03-12 09:55:17 -0700

    chore: remove redundant DyeColor from bed
    It's not even used.

--- xplat/src/main/java/gay/pridecraft/joy/block/entity/JoyBedBlockEntity.java
+++ xplat/src/main/java/gay/pridecraft/joy/block/entity/JoyBedBlockEntity.java
@@ -4,15 +4,11 @@ import gay.pridecraft.joy.registry.JoyBlockEntityTypes;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
-import net.minecraft.util.DyeColor;
 import net.minecraft.util.math.BlockPos;
 
 public class JoyBedBlockEntity extends BlockEntity {
-    public DyeColor color;
-
     public JoyBedBlockEntity(BlockPos pos, BlockState state) {
         super(JoyBlockEntityTypes.JOY_BED_BLOCK_ENTITY, pos, state);
-        this.color = DyeColor.WHITE;
     }
 
     public BlockEntityUpdateS2CPacket toUpdatePacket() {
